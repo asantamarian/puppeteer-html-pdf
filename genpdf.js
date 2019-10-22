12cb3194c45531ab8c9da8763153c97cef6a87dc
andres <andres.santamaria@fch.cl>
2019-10-22 16:10:47 -0300

    url local

--- genpdf.js
+++ genpdf.js
@@ -4,14 +4,14 @@ async function generatePDF() {
   
   const browser = await puppeteer.launch({ ignoreHTTPSErrors: true,headless: true });
   const page = await browser.newPage();
-  await page.goto('http://localhost:3335/Desempeno/Informe/exportDesempeno?pdf=1', {waitUntil: 'networkidle2'});
+  await page.goto('http://localhost:3335/Acreditacion/Informe/pdf?procesoPersona=9d3fade8-d58d-11e9-a12e-12003a462036&cd=1', {waitUntil: 'networkidle2'});
   await page.emulateMedia('screen');
   
   const pdf = await page.pdf({ 
-    path:"dd.pdf",
+    path:"new.pdf",
     //format: 'A4',
     printBackground:true,
-    width: '1980px' , height: '1080px',
+    width: '1980px' , height: '1200px',
     landscape:false
     }
       );
